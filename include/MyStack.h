2fb8b1a8abb008ab80938102af719633da03eede
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-01-11 21:57:48 +0300

    Add files via upload

---
+++ include/MyStack.h
@@ -0,0 +1,53 @@
+﻿template<typename Stack>
+class MyStack
+{
+private:
+	Stack* stackPtr; // указатель на стек
+	int size; // размер стека
+	Stack top; // вершина стека
+public:
+	MyStack() {
+		this->size = 0;
+		this->stackPtr = 0;
+		this->top = 0;
+	}
+	MyStack(int size) {
+		this->size = size;
+		this->stackPtr = new Stack[size];
+		this->top = 0;
+	}
+	~MyStack()
+	{
+		delete[] stackPtr; // удаляем стек
+	};
+	void push(const Stack phead) {
+		++top;
+		stackPtr[top] = phead; // помещаем элемент в стек
+	}; 
+	Stack pop() {
+		Stack value;
+		if (top != -1) {
+			value = stackPtr[top];
+			stackPtr[top] = 0; // удаляем элемент из стека
+			--top;
+		}
+		else return 0;
+		return value;
+	}; // удалить из стека элемент
+	bool isFull() {
+		if (top == 0) return true;
+		else {
+			if (top == size) return true;
+			return false;
+		}
+	}
+	bool isEmpty() {
+		if (top == 0) return true;
+		return false;
+	}
+	Stack get() {
+		return stackPtr[top];
+	};
+};
+
+
