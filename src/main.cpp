191703406c1ff0de42e508d423b204e816d08bfd
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2021-01-11 23:11:29 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 #include <iostream>
 
 int main() {
-    MyStack<int> st(5);
-    int temp = 0;
-    for (int i = 0; i < 5; i++) {
-        std::cin >> temp;
-        st.push(temp);
-    }
+  MyStack<int> st(5);
+  int temp = 0;
+  for (int i = 0; i < 5; i++) {
+    std::cin >> temp;
+    st.push(temp);
+  }
 
-    while (!st.isEmpty())
-        std::cout << st.pop() << " ";
-    return 0;
+  while (!st.isEmpty())
+    std::cout << st.pop() << " ";
+  return 0;
 }
