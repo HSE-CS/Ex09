5cc92cedf6bde5e9ca86bfdeb06f26ee1e33a965
Ilya Urtyokov <urtilya@gmail.com>
2021-01-11 16:36:14 +0300

    first try

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+// Copyright 2020 Ilya Urtyukov
+#include <iostream>
+#include "MyStack.h"
+
+int main() {
+    MyStack<int> st(10);
+    st.push(rand());
+    st.push(rand());
+    st.push(rand());
+    while(!st.isEmpty())
+       std::cout << st.pop() <<" ";
+    return 0;
+}
+
