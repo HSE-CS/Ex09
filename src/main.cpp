43c053ba65ecbbaac2af66c91e1ccfa2de30b8e1
Osmanov Islam <osmangov@yandex.ru>
2021-01-11 21:24:05 +0300

    Fix spaces

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,25 @@
 // Copyright 2021 Islam Osmanov
 #include <iostream>
 #include "MyStack.h"
-#include <stdio.h>
 #include <string>
 
 
 int main() {
-	MyStack<int> Stack(3);
-	Stack.push(19);
-	Stack.push(20);
-	std::cout << Stack.isFull() << std::endl;
-	std::cout << Stack.get() << std::endl;
-	Stack.push(1000);
-	std::cout << Stack.isEmpty() << std::endl;
-	std::cout << Stack.isFull() << std::endl;
-	Stack.pop();
-	std::cout << Stack.get() << std::endl;
+  MyStack<int> Stack(3);
+  Stack.push(19);
+  Stack.push(20);
+  std::cout << Stack.isFull() << std::endl;
+  std::cout << Stack.get() << std::endl;
+  Stack.push(1000);
+  std::cout << Stack.isEmpty() << std::endl;
+  std::cout << Stack.isFull() << std::endl;
+  Stack.pop();
+  std::cout << Stack.get() << std::endl;
 
-	MyStack<std::string> OtherStack(2);
-	OtherStack.push("Manchester United");
-	OtherStack.push("Manchester United");
-	OtherStack.pop();
-	std::cout << OtherStack.isFull() << std::endl;
-	return 0;
+  MyStack<std::string> OtherStack(2);
+  OtherStack.push("Manchester United");
+  OtherStack.push("Manchester United");
+  OtherStack.pop();
+  std::cout << OtherStack.isFull() << std::endl;
+  return 0;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -4,79 +4,79 @@
 #include "MyStack.h"
 
 TEST(MyStackTest, test1) {
-	MyStack<int> Stack(1);
-	Stack.push(20);
-	EXPECT_EQ(false, Stack.isEmpty());
+  MyStack<int> Stack(1);
+  Stack.push(20);
+  EXPECT_EQ(false, Stack.isEmpty());
 }
 
 TEST(MyStackTest, test2) {
-	MyStack<int> Stack(3);
-	Stack.push(20);
-	Stack.push(30);
-	Stack.push(40);
-	EXPECT_EQ(true, Stack.isFull());
+  MyStack<int> Stack(3);
+  Stack.push(20);
+  Stack.push(30);
+  Stack.push(40);
+  EXPECT_EQ(true, Stack.isFull());
 }
 
 TEST(MyStackTest, test3) {
-	MyStack<std::string> Stack(2);
-	Stack.push("Hello");
-	Stack.push("World");
-	Stack.pop();
-	EXPECT_EQ(false, Stack.isFull());
+  MyStack<std::string> Stack(2);
+  Stack.push("Hello");
+  Stack.push("World");
+  Stack.pop();
+  EXPECT_EQ(false, Stack.isFull());
 }
 
 TEST(MyStackTest, test4) {
-	MyStack<int> Stack(2);
-	Stack.push(1024);
-	Stack.push(312);
-	EXPECT_EQ(312, Stack.get());
+  MyStack<int> Stack(2);
+  Stack.push(1024);
+  Stack.push(312);
+  EXPECT_EQ(312, Stack.get());
 }
 
 TEST(MyStackTest, test5) {
-	MyStack<std::string> Stack(3);
-	Stack.push("C++");
-	Stack.push("Python");
-	EXPECT_EQ("Python", Stack.get());
+  MyStack<std::string> Stack(3);
+  Stack.push("C++");
+  Stack.push("Python");
+  EXPECT_EQ("Python", Stack.get());
 }
 
 TEST(MyStackTest, test6) {
-	MyStack<std::string> Stack(1);
-	Stack.push("C#");
-	Stack.pop();
-	EXPECT_EQ(true, Stack.isEmpty());
+  MyStack<std::string> Stack(1);
+  Stack.push("C#");
+  Stack.pop();
+  EXPECT_EQ(true, Stack.isEmpty());
 }
 
 TEST(MyStackTest, test7) {
-	MyStack<int> Stack(3);
-	Stack.push(123);
-	Stack.pop();
-	Stack.push(1234);
-	EXPECT_EQ(false, Stack.isFull());
+  MyStack<int> Stack(3);
+  Stack.push(123);
+  Stack.pop();
+  Stack.push(1234);
+  EXPECT_EQ(false, Stack.isFull());
 }
 
 TEST(MyStackTest, test8) {
-	MyStack<int> Stack(4);
-	Stack.push(1);
-	Stack.pop();
-	Stack.push(2);
-	Stack.push(3);
-	EXPECT_EQ(3, Stack.get());
+  MyStack<int> Stack(4);
+  Stack.push(1);
+  Stack.pop();
+  Stack.push(2);
+  Stack.push(3);
+  EXPECT_EQ(3, Stack.get());
 }
 
 TEST(MyStackTest, test9) {
-	MyStack<char> Stack(3);
-	Stack.push('a');
-	Stack.push('2');
-	Stack.push('3');
-	
-	EXPECT_EQ(true, Stack.isFull());
+  MyStack<char> Stack(3);
+  Stack.push('a');
+  Stack.push('2');
+  Stack.push('3');
+  
+  EXPECT_EQ(true, Stack.isFull());
 }
 
 TEST(MyStackTest, test10) {
-	MyStack<char> Stack(2);
-	Stack.push('c');
-	Stack.push('4');
-	Stack.pop();
+  MyStack<char> Stack(2);
+  Stack.push('c');
+  Stack.push('4');
+  Stack.pop();
 
-	EXPECT_EQ('c', Stack.get());
+  EXPECT_EQ('c', Stack.get());
 }
