cbf0f065945bc3cf45ae34566ec812083762aa5c
EgorBuzanov <egor4ek007@gmail.com>
2021-01-11 22:54:14 +0300

    Egor Buzanov

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,26 @@
 // Copyright 2021 Egor Buzanov
 
-#include <cstdlib>
 #include <iostream>
 
 #include "../include/MyStack.h"
 
 int main() {
   MyStack<int> st(10);
-  st.push(rand());
-  st.push(rand());
-  st.push(rand());
+  st.push(1);
+  st.push(2);
+  st.push(3);
 
   MyStack<int> new_stack(st);
 
   while (!st.isEmpty()) {
     std::cout << st.pop() << " ";
-  };
+  }
 
   std::cout << std::endl;
   std::cout << new_stack.get() << " " << new_stack.isFull() << std::endl;
   while (!st.isEmpty()) {
     std::cout << st.pop() << " ";
-  };
+  }
   std::cout << std::endl;
 
   return 0;
