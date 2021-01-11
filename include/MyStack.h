90bba50627e7065a8855439fc46ad097f914bbb0
__dolt_coder__ <ihackit595@gmail.com>
2021-01-11 20:13:14 +0300

    wrote all functions

--- include/MyStack.h
+++ include/MyStack.h
@@ -2,19 +2,50 @@
 #ifndef INCLUDE_MYSTACK_H_
 #define INCLUDE_MYSTACK_H_
 
-template< type Typ>
+template<class Typ>
 class MyStack {
  private:
+    int amountOfElements = 0;
     int stackSize;
     Typ* stackArray;
+
  public:
-    explicit MyStack(int size);
-    MyStack(const MyStack &stack);
-    ~MyStack();
-    void push(Typ el);
-    Typ pop();
-    Typ get();
-    bool isFull();
-    bool isEmpty();
-}
-#endif INCLUDE_MYSTACK_H_
+    explicit MyStack(int size) {
+        stackSize = size;
+        stackArray = new Typ[stackSize];
+    }
+    MyStack(const MyStack &stack):
+        amountOfElements(stack.amountOfElements),
+        stackSize(stack.stackSize), stackArray(stack.stackArray) {}
+
+    ~MyStack() {
+        delete[] stackArray;
+        stackSize = 0;
+    }
+    void push(Typ el) {
+        stackArray[amountOfElements] = el;
+        amountOfElements++;
+    }
+    Typ pop() {
+        Typ temp = stackArray[amountOfElements-1];
+        Typ *resized = new Typ[amountOfElements - 1];
+        for (int i = 0; i < amountOfElements - 1; i ++)
+            resized[i] = stackArray[i];
+
+        delete[] stackArray;
+        amountOfElements--;
+        stackArray = resized;
+        return temp;
+        }
+    Typ get() const {
+        return stackArray[amountOfElements-1];
+    }
+    bool isFull() const {
+        return amountOfElements == stackSize;
+    }
+    bool isEmpty() const {
+        return amountOfElements == 0;
+    }
+};
+
+#endif  // INCLUDE_MYSTACK_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 // Copyright 2020 Bogomazov
 #include<iostream>
-#include "../include/MyStack.h";
+#include "../include/MyStack.h"
 int main() {
     MyStack<int> st(10);
+    st.push(rand());
+    st.push(rand());
+    st.push(rand());
+
+    while (!st.isEmpty())
+       std::cout << st.pop() <<" ";
     return 0;
 }
