7e5c99e2f96e8a1113852514477d7eecfc6c06e0
Ilya Urtyokov <urtilya@gmail.com>
2021-01-11 16:56:39 +0300

    fixed3

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 
 int main() {
     MyStack<int> st(10);
-    st.push(rand_r());
-    st.push(rand_r());
-    st.push(rand_r());
-    while(!st.isEmpty())
+    st.push(5);
+    st.push(6);
+    st.push(7);
+    while (!st.isEmpty())
        std::cout << st.pop() <<" ";
     return 0;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -22,47 +22,47 @@ TEST(MyStackTest, test3) {
 
 TEST(MyStackTest, test4) {
   MyStack<int> st(3);
-  st.push(rand_r());
-  st.push(rand_r());
+  st.push(25);
+  st.push(40);
   st.push(10);
   EXPECT_EQ(10, st.get());
 }
 
 TEST(MyStackTest, test5) {
   MyStack<int> st(3);
-  st.push(rand_r());
-  st.push(rand_r());
+  st.push(25);
+  st.push(40);
   st.push(10);
   EXPECT_EQ(10, st.pop());
 }
 
 TEST(MyStackTest, test6) {
   MyStack<int> st(3);
-  st.push(rand_r());
-  st.push(rand_r());
+  st.push(25);
+  st.push(40);
   st.push(10);
   EXPECT_EQ(true, st.isFull());
 }
 
 TEST(MyStackTest, test7) {
   MyStack<int> st(3);
-  st.push(rand_r());
-  st.push(rand_r());
+  st.push(25);
+  st.push(40);
   EXPECT_EQ(false, st.isFull());
 }
 
 TEST(MyStackTest, test8) {
   MyStack<int> st(3);
-  st.push(rand_r());
-  st.push(rand_r());
+  st.push(25);
+  st.push(40);
   st.push(10);
   EXPECT_EQ(false, st.isEmpty());
 }
 
 TEST(MyStackTest, test9) {
   MyStack<int> st(3);
-  st.push(rand_r());
-  st.push(rand_r());
+  st.push(25);
+  st.push(40);
   EXPECT_EQ(false, st.isEmpty());
 }
 
