65b102f9bd8199e4510ea88de3cc44bc042ad49a
KuznetsovMikhail <KuznetsovMikhail>
2021-01-11 14:01:21 +0300

    add MyStack.h and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 // Copyright 2021 Kuznetsov Mikhail
 
 #include "MyStack.h"
+#include <iostream>
+#include <stdlib.h>
 
 int main() {
-
+  MyStack<int> st(8);
+  while(!st.isFull()) {
+    st.push(rand());
+    std::cout << st.get() << " ";
+  }
+  MyStack<int> new_st(st);
+  std::cout << std::endl;
+  while(!new_st.isEmpty())
+    std::cout << new_st.pop() << " ";
+  return 0;
 }
