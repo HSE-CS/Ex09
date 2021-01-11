3a3fcabf77d59e57dda188558fd1719a23141c12
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2021-01-11 23:10:09 +0300

    Update MyStack.h

--- include/MyStack.h
+++ include/MyStack.h
@@ -6,62 +6,61 @@
 template<class T>
 class MyStack {
  private:
-	 int top;
-	 int size;
-	 T* stack;
+  int top;
+  int size;
+  T* stack;
  public:
-	 explicit MyStack(int sizeIn) {
-		 top = 0;
-		 size = sizeIn;
-		 stack = new T[size];
-	 }
+  explicit MyStack(int sizeIn) {
+    top = 0;
+    size = sizeIn;
+    stack = new T[size];
+  }
 
-	 MyStack(const MyStack& stack2) {
-		 top = stack2.top;
-		 size = stack2.size;
-		 stack = new T[size];
-		 for (int i = 0; i < size; i++)
-			 stack[i] = stack2.stack[i];
-	 }
+  MyStack(const MyStack& stack2) {
+    top = stack2.top;
+    size = stack2.size;
+    stack = new T[size];
+    for (int i = 0; i < size; i++)
+    stack[i] = stack2.stack[i];
+  }
 
-	 ~MyStack() {
-		 delete[] stack;
-	 }
+  ~MyStack() {
+    delete[] stack;
+  }
 
-	 T get() const {
-		 if (top != 0) {
-			 return stack[top - 1];
-		 }
-	 }
+  T get() const {
+    if (top != 0) {
+    return stack[top - 1];
+    }
+  }
 
-	 T pop() {
-		 if (top != 0) {
-			 top--;
-			 return stack[top];
-		 }
-	 }
+  T pop() {
+    if (top != 0) {
+      top--;
+      return stack[top];
+    }
+  }
 
-	 void push(T elem) {
-		 if (top < size) {
-			 stack[top] = elem;
-			 top++;
-		 }
-	 }
+  void push(T elem) {
+    if (top < size) {
+      stack[top] = elem;
+      top++;
+    }
+  }
 
-	 bool isFull() const {
-		 if (top == size)
-			 return true;
-		 else
-			 return false;
-	 }
+  bool isFull() const {
+    if (top == size)
+      return true;
+    else
+      return false;
+    }
 
-	 bool isEmpty() const {
-		 if (top == 0)
-			 return true;
-		 else
-			 return false;
-	 }
+  bool isEmpty() const {
+    if (top == 0)
+      return true;
+    else
+      return false;
+  }
 };
-
 
 #endif  // INCLUDE_MYSTACK_H_
