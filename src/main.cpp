58d90e3e384220ac175d7514078d11181db700cb
sand user <sand09.me@gmail.com>
2021-01-11 23:14:40 +0300

    Linter fix

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 #include <iostream>
 #include "MyStack.h"
 
-int main(){
+int main() {
   MyStack<int> st(0);
   st.push(1);
   st.push(2);
   st.push(3);
 
-  while(!st.isEmpty())
+  while (!st.isEmpty())
      std::cout << st.pop() <<" ";
   return 0;
 }
