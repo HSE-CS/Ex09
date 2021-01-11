eedfec2216e60f9ac82c3cde3dcf89bca357194f
GlebNeshchetkin <glebneshchetkin@gmail.com>
2021-01-11 20:45:55 +0300

    FirstTest

--- include/MyStack.h
+++ include/MyStack.h
@@ -2,44 +2,44 @@
 #ifndef INCLUDE_MYSTACK_H_
 #define INCLUDE_MYSTACK_H_
 #include <iostream>
-#include <string.h>
 template <class T>
 class MyStack {
-private:
+ private:
     T* stack;
-    long long size;
-    long long position = 0;
-public:
-    explicit MyStack(long long size_) {
+    int size;
+    int position = 0;
+ 
+ public:
+    explicit MyStack(int size_) {
         size = size_;
         stack = new T[size];
-    };
+    }
     MyStack(const MyStack& st) {
         size = st.size;
         stack = new T[size];
         memcpy(stack, st.stack, size);
         position = st.position;
-    };
+    }
     ~MyStack() {
         delete[] stack;
-	};
+    }
     T& get() {
-        long long position_ = position - 1;
+        int position_ = position - 1;
         return stack[position_];
-    };
-    long long getSize() {
+    }
+    int getSize() {
         return size;
-    };
+    }
     T& pop() {
         position--;
         return stack[position];
-    };
+    }
     void push(double x) {
         if (position < size) {
             stack[position] = x;
             position++;
         }
-        else if (position == size) {}
+        if (position == size) {}
     }
     bool isFull() {
         if (position == size) {
