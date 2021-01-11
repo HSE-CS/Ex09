95d2678dafd4b3ae06d3eec23769a9b25182314f
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-01-11 22:04:16 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,56 +7,56 @@ TEST(MyStackTest, test1) {
 	EXPECT_EQ(true, st.isEmpty());
 }
 
-TEST(MyStackTest, test1) {
+TEST(MyStackTest, test2) {
 	MyStack<int> st(10);
 	st.push(12);
 	EXPECT_EQ(false, st.isEmpty());
 }
 
-TEST(MyStackTest, test1) {
+TEST(MyStackTest, test3) {
 	MyStack<int> st(10);
 	st.push(12);
 	st.pop();
 	EXPECT_EQ(true, st.isEmpty());
 }
 
-TEST(MyStackTest, test1) {
+TEST(MyStackTest, test4) {
 	MyStack<int> st(10);
 	st.push(12);
 	EXPECT_EQ(false, st.isFull());
 }
 
-TEST(MyStackTest, test1) {
+TEST(MyStackTest, test5) {
 	MyStack<int> st(1);
 	st.push(12);
 	EXPECT_EQ(true, st.isFull());
 }
 
-TEST(MyStackTest, test1) {
+TEST(MyStackTest, test6) {
 	MyStack<int> st(1);
 	st.push(12);
 	EXPECT_EQ(12, st.get());
 }
 
-TEST(MyStackTest, test1) {
+TEST(MyStackTest, test7) {
 	MyStack<int> st(10);
 	EXPECT_EQ(false, st.isFull());
 }
 
-TEST(MyStackTest, test1) {
+TEST(MyStackTest, test8) {
 	MyStack<int> st(10);
 	st.push(12);
 	EXPECT_EQ(false, st.isEmpty());
 }
 
-TEST(MyStackTest, test1) {
+TEST(MyStackTest, test9) {
 	MyStack<int> st(1);
 	st.push(12);
 	st.push(34);
 	EXPECT_EQ(34, st.get());
 }
 
-TEST(MyStackTest, test1) {
+TEST(MyStackTest, test10) {
 	MyStack<int> st(1);
 	st.push(2112);
 	st.pop();
