6dfad122424a06f0fedb81f9b414814b7d3d3d2f
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-01-11 22:15:53 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
+// Copyright 2020 Baklanov
 #include "MyStack.h"
 #include <iostream>
 #include <random>
 
 int main()
 {
-	MyStack<int> st(10);
-	st.push(rand());
-	st.push(rand());
-	st.push(rand());
+  MyStack<int> st(10);
+  st.push(rand());
+  st.push(rand());
+  st.push(rand());
 
-	while (!st.isEmpty())
-		std::cout << st.pop() << " ";
-	return 0;
+  while (!st.isEmpty())
+    std::cout << st.pop() << " ";
+    return 0;
 }
