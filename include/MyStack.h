2af413c73bb72efe0a66a1d672e0913255aed6a4
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-01-11 22:46:46 +0300

    Update MyStack.h

--- include/MyStack.h
+++ include/MyStack.h
@@ -1,13 +1,16 @@
 // Copyright 2020 Baklanov
+
 #ifndef INCLUDE_MYSTACK_H_ 
+
 #define INCLUDE_MYSTACK_H_ 
+
 template<typename Stack>
 class MyStack {
-private:
-Stack* stackPtr; // указатель на стек
-int size; // размер стека
-Stack top; // вершина стека
-public:
+ private:
+Stack* stackPtr;  // указатель на стек
+int size;  // размер стека
+Stack top;  // вершина стека
+ public:
 MyStack() {
 this->size = 0;
 this->stackPtr = 0;
@@ -24,22 +27,22 @@ delete[] stackPtr; // удаляем стек
 void push(const Stack phead) {
 ++top;
 stackPtr[top] = phead; // помещаем элемент в стек
-}; 
+};
 Stack pop() {
 Stack value;
 if (top != -1) {
-value = stackPtr[top];
-stackPtr[top] = 0; // удаляем элемент из стека
---top;
+	value = stackPtr[top];
+	stackPtr[top] = 0; // удаляем элемент из стека
+	--top;
 }
 else return 0;
 return value;
-}; 
+};
 bool isFull() {
 if (top == 0) return true;
 else {
-if (top == size) return true;
-return false;
+	if (top == size) return true;
+	return false;
 }
 }
 bool isEmpty() {
@@ -51,6 +54,6 @@ return stackPtr[top];
 };
 };
 
-#endif
+#endif  // INCLUDE_MYSTACK_H_
 
 
