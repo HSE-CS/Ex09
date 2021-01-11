7c0de131577aa6ed506bd927d26f730b2fceb1b6
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-01-11 19:53:45 +0300

    less tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -15,14 +15,6 @@ TEST(MyStackTest, test2) {
   EXPECT_EQ(false, stack.isEmpty());
 }
 
-TEST(MyStackTest, test5) {
-  MyStack<std::string> stack(10);
-  stack.push("22");
-  stack.push("22");
-  stack.push("22");
-  EXPECT_EQ("22", stack.get());
-}
-
 TEST(MyStackTest, test6) {
   MyStack<std::string> stack(10);
   stack.push("22");
@@ -41,11 +33,6 @@ TEST(MyStackTest, test8) {
   EXPECT_EQ("23", stack.get());
 }
 
-TEST(MyStackTest, test11) {
-  MyStack<int> stack(5);
-  EXPECT_EQ(false, stack.isFull());
-}
-
 TEST(MyStackTest, test12) {
   MyStack<int> stack(5);
   const MyStack<int> &stack2 = stack;
@@ -58,14 +45,6 @@ TEST(MyStackTest, test14) {
   stack.push(40);
   stack.push(10);
   EXPECT_EQ(10, stack.pop());
-}
-
-TEST(MyStackTest, test15) {
-  MyStack<int> stack(3);
-  stack.push(25);
-  stack.push(40);
-  stack.push(10);
-  EXPECT_EQ(true, stack.isFull());
 }
 
 TEST(MyStackTest, test17) {
@@ -129,29 +108,10 @@ TEST(MyStackTest, test28) {
   EXPECT_EQ(true, stack.isEmpty());
 }
 
-TEST(MyStackTest, test30) {
-  MyStack<int> stack(5);
-  stack.push(1);
-  stack.push(1);
-  stack.push(2);
-  stack.push(5);
-  stack.pop();
-  int x = stack.pop();
-  EXPECT_EQ(2, x);
-}
-
 TEST(MyStackTest, test31) {
   MyStack<char> stack(5);
   stack.push('z');
   EXPECT_EQ('z', stack.get());
-}
-
-TEST(MyStackTest, test33) {
-  MyStack<char> stack(5);
-  stack.push('z');
-  stack.push('a');
-  MyStack<char> stack2(stack);
-  EXPECT_EQ('a', stack2.get());
 }
 
 TEST(MyStackTest, test34) {
