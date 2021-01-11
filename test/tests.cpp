da7958c73652897d1ee9515ecdfe79db7f66dd11
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-01-11 21:58:48 +0300

    Add files via upload

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,11 +1,64 @@
-// Copyright 2020 GHA Test Team
-#include "MyStack.h"
-
+#include "../include/MyStack.h"
+#include <cstring>
 #include <gtest/gtest.h>
-#include <string>
 
 TEST(MyStackTest, test1) {
-  MyStack<int> st(5);
-  EXPECT_EQ(true, st.isEmpty());
+	MyStack<int> st(10);
+	EXPECT_EQ(true, st.isEmpty());
 }
 
+TEST(MyStackTest, test1) {
+	MyStack<int> st(10);
+	st.push(12);
+	EXPECT_EQ(false, st.isEmpty());
+}
+
+TEST(MyStackTest, test1) {
+	MyStack<int> st(10);
+	st.push(12);
+	st.pop();
+	EXPECT_EQ(true, st.isEmpty());
+}
+
+TEST(MyStackTest, test1) {
+	MyStack<int> st(10);
+	st.push(12);
+	EXPECT_EQ(false, st.isFull());
+}
+
+TEST(MyStackTest, test1) {
+	MyStack<int> st(1);
+	st.push(12);
+	EXPECT_EQ(true, st.isFull());
+}
+
+TEST(MyStackTest, test1) {
+	MyStack<int> st(1);
+	st.push(12);
+	EXPECT_EQ(12, st.get());
+}
+
+TEST(MyStackTest, test1) {
+	MyStack<int> st(10);
+	EXPECT_EQ(false, st.isFull());
+}
+
+TEST(MyStackTest, test1) {
+	MyStack<int> st(10);
+	st.push(12);
+	EXPECT_EQ(false, st.isEmpty());
+}
+
+TEST(MyStackTest, test1) {
+	MyStack<int> st(1);
+	st.push(12);
+	st.push(34);
+	EXPECT_EQ(34, st.get());
+}
+
+TEST(MyStackTest, test1) {
+	MyStack<int> st(1);
+	st.push(2112);
+	st.pop();
+	EXPECT_EQ(true, st.isEmpty());
+}
