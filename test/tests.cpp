0bab73b033344da26fcaf8d76b83a6c3fa529cae
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-01-11 22:24:53 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,65 +1,65 @@
 // Copyright 2020 Baklanov
 #include "../include/MyStack.h"
-#include <cstring>
+#include <string>
 #include <gtest/gtest.h>
 
 TEST(MyStackTest, test1) {
-	MyStack<int> st(10);
-	EXPECT_EQ(true, st.isEmpty());
+  MyStack<int> st(10);
+  EXPECT_EQ(true, st.isEmpty());
 }
 
 TEST(MyStackTest, test2) {
-	MyStack<int> st(10);
-	st.push(12);
-	EXPECT_EQ(false, st.isEmpty());
+  MyStack<int> st(10);
+  st.push(12);
+  EXPECT_EQ(false, st.isEmpty());
 }
 
 TEST(MyStackTest, test3) {
-	MyStack<int> st(10);
-	st.push(12);
-	st.pop();
-	EXPECT_EQ(true, st.isEmpty());
+  MyStack<int> st(10);
+  st.push(12);
+  st.pop();
+  EXPECT_EQ(true, st.isEmpty());
 }
 
 TEST(MyStackTest, test4) {
-	MyStack<int> st(10);
-	st.push(12);
-	EXPECT_EQ(false, st.isFull());
+  MyStack<int> st(10);
+  st.push(12);
+  EXPECT_EQ(false, st.isFull());
 }
 
 TEST(MyStackTest, test5) {
-	MyStack<int> st(1);
-	st.push(12);
-	EXPECT_EQ(true, st.isFull());
+  MyStack<int> st(1);
+  st.push(12);
+  EXPECT_EQ(true, st.isFull());
 }
 
 TEST(MyStackTest, test6) {
-	MyStack<int> st(1);
-	st.push(12);
-	EXPECT_EQ(12, st.get());
+  MyStack<int> st(1);
+  st.push(12);
+  EXPECT_EQ(12, st.get());
 }
 
 TEST(MyStackTest, test7) {
-	MyStack<int> st(10);
-	EXPECT_EQ(false, st.isFull());
+  MyStack<int> st(10);
+  EXPECT_EQ(false, st.isFull());
 }
 
 TEST(MyStackTest, test8) {
-	MyStack<int> st(10);
-	st.push(12);
-	EXPECT_EQ(false, st.isEmpty());
+  MyStack<int> st(10);
+  st.push(12);
+  EXPECT_EQ(false, st.isEmpty());
 }
 
 TEST(MyStackTest, test9) {
-	MyStack<int> st(1);
-	st.push(12);
-	st.push(34);
-	EXPECT_EQ(34, st.get());
+  MyStack<int> st(1);
+  st.push(12);
+  st.push(34);
+  EXPECT_EQ(34, st.get());
 }
 
 TEST(MyStackTest, test10) {
-	MyStack<int> st(1);
-	st.push(2112);
-	st.pop();
-	EXPECT_EQ(true, st.isEmpty());
+  MyStack<int> st(1);
+  st.push(2112);
+  st.pop();
+  EXPECT_EQ(true, st.isEmpty());
 }
