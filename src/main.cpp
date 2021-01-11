d2d71ef3bfc525d6067fd26c21d0cbd31dde8aa8
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-01-11 21:58:16 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "MyStack.h"
+#include <iostream>
+#include <random>
+
+int main()
+{
+	MyStack<int> st(10);
+	st.push(rand());
+	st.push(rand());
+	st.push(rand());
+
+	while (!st.isEmpty())
+		std::cout << st.pop() << " ";
+	return 0;
+}
