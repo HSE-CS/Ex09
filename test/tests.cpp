c7cffbabe7b6a8123adcd333ceb5265732c4c2bc
MalininDD <54958481+MalininDD@users.noreply.github.com>
2021-01-12 13:07:51 +0300

    Add files via upload

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,23 +1,25 @@
+// Copyright 2020 GHA Test Team
 #include "MyStack.h"
+
 #include <gtest/gtest.h>
 #include <string>
 
-TEST(MyTest, test1) {
+TEST(MyStackTest, test1) {
     MyStack<int> st(3);
     EXPECT_EQ(true, st.isEmpty());
 }
 
-TEST(MyTest, test2) {
+TEST(MyStackTest, test2) {
     MyStack<int> st(1);
     st.push(8);
     EXPECT_EQ(true, st.isFull());
 }
-TEST(MyTest, test3) {
+TEST(MyStackTest, test3) {
     MyStack<int> st(2);
     st.push(228);
     EXPECT_EQ(228,st.get());
 }
-TEST(MyTest, test4) {
+TEST(MyStackTest, test4) {
     MyStack<double> st(4);
     st.push(2.228);
     st.push(1.337);
@@ -25,19 +27,19 @@ TEST(MyTest, test4) {
     st.push(1.437);
     EXPECT_EQ(1.437,st.get());
 }
-TEST(MyTest, test5) {
+TEST(MyStackTest, test5) {
     MyStack<double> st(1);
     st.push(1.8);
     EXPECT_EQ(1.8, st.get());
 }
-TEST(MyTest, test6) {
+TEST(MyStackTest, test6) {
     MyStack<char> st(3);
     st.push('h');
     st.push('y');
     st.push('p');
     EXPECT_EQ('p', st.get());
 }
-TEST(MyTest, test7) {
+TEST(MyStackTest, test7) {
     MyStack<char> st(3);
     st.push('h');
     st.push('y');
@@ -46,14 +48,14 @@ TEST(MyTest, test7) {
     st.pop();
     EXPECT_EQ('h', st.get());
 }
-TEST(MyTest, test8) {
-    MyStack<string> st(4);
+TEST(MyStackTest, test8) {
+    MyStack<std::string> st(4);
     st.push("Hi");
     st.pop();
     st.push("Welcome");
     EXPECT_EQ("Welcome", st.get());
 }
-TEST(MyTest, test9) {
+TEST(MyStackTest, test9) {
     MyStack<int> st(4);
     st.push(8);
     st.push(3);
@@ -61,7 +63,7 @@ TEST(MyTest, test9) {
     st.pop();
     EXPECT_EQ(false, st.isFull());
 }
-TEST(MyTest, test10) {
+TEST(MyStackTest, test10) {
     MyStack<int> st(4);
     st.push(8);
     st.push(3);
