dac43231590c8f7aaf2459b8148cb47afbf3c7f0
Ksuvot <ksuvot36@yandex.ru>
2021-01-12 09:30:13 +0300

    add files

---
+++ include/MyStack.h
@@ -0,0 +1,21 @@
+//  Copyright © 2021 Ksuvot. All rights reserved.
+
+#ifndef EX09_INCLUDE_MYSTACK_H_
+#define EX09_INCLUDE_MYSTACK_H_
+
+/* TODO:
+ * конструктор с одним параметром.
+ * конструктор копирования.
+ * деструктор.
+ * get() const - возвращение эелемента на вершине стека (без изменения состояния последнего).
+ * pop() - выталкивание элемента из стека.
+ * push() - добавление элемента в стек.
+ * isFull() const - проверка заполненности стека.
+ * isEmpty() const - проверка пустоты стека.
+*/
+
+class MyStack{
+ private:
+};
+
+#endif //EX09_INCLUDE_MYSTACK_H_
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//  Copyright © 2021 Ksuvot. All rights reserved.
+#include "MyStack.h"
+
+int main()
+{
+  MyStack<int> st(10);
+  st.push(rand());
+  st.push(rand());
+  st.push(rand());
+
+  while(!st.isEmpty())
+    std::cout << st.pop() <<" ";
+  return 0;
+}
