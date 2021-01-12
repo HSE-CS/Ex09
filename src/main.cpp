3176e7820e79d353ac3c84f94628a6615751f431
Lavrov-Artyom <artem3sandy@gmail.com>
2021-01-12 10:01:23 +0300

    Third.1

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,13 @@
 
 
 int main() {
- MyStack<int> stack(10);
- stack.push(1);
- std::cout << stack.get() << std::endl;
- stack.push(2);
- std::cout << stack.get() << std::endl;
- std::cout << stack.pop() << std::endl;
- std::cout << stack.get() << std::endl;
- MyStack<int> stack2(stack);
- std::cout << stack2.get() << std::endl;
+  MyStack<int> stack(10);
+  stack.push(1);
+  std::cout << stack.get() << std::endl;
+  stack.push(2);
+  std::cout << stack.get() << std::endl;
+  std::cout << stack.pop() << std::endl;
+  std::cout << stack.get() << std::endl;
+  MyStack<int> stack2(stack);
+  std::cout << stack2.get() << std::endl;
 }
