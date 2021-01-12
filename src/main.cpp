e1fca1bc31f312cecb939d939e1e704463303301
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2021-01-13 02:29:00 +0300

    fix typo

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
-// Copyright 2020 ArinaMonicheva
-#include "MyStack.h"
+// Copyright 2021 ArinaMonicheva
 #include "time.h"
 #include <iostream>
+#include "MyStack.h"
 
 int main() {
   srand(time(0));
 
   MyStack<int> st(10);
 
-  std::cout << "Full or empty: " << st.isFull() << ' ' << st.isEmpty() << std::endl;
+  std::cout << "Full or empty: " << st.isFull() << ' ';
+  std::cout << st.isEmpty() << std::endl;
 
-  st.push(rand());
-  st.push(rand());
-  st.push(rand());
+  st.push(rand_r());
+  st.push(rand_r());
+  st.push(rand_r());
 
-  std::cout << "Full or empty: " << st.isFull() << ' ' << st.isEmpty() << std::endl;
+  std::cout << "Full or empty: " << st.isFull() << ' ';
+  std::cout << st.isEmpty() << std::endl;
 
   std::cout << st.get() << std::endl;
   std::cout << st.pop() << std::endl;
@@ -24,28 +26,32 @@ int main() {
     std::cout << st.pop() << ' ';
   }
 
-  std::cout << "Full or empty: " << st.isFull() << ' ' << st.isEmpty() << std::endl;
+  std::cout << "Full or empty: " << st.isFull() << ' ';
+  std::cout << st.isEmpty() << std::endl;
 
-  while (!st.isFull()) {
-    st.push(rand());
+  while ( !st.isFull()) {
+    st.push(rand_r());
   }
 
-  std::cout << "Full or empty: " << st.isFull() << ' ' << st.isEmpty() << std::endl;
+  std::cout << "Full or empty: " << st.isFull() << ' ';
+  std::cout << st.isEmpty() << std::endl;
 
   std::cout << st.get() << std::endl;
 
   MyStack<int> newSt(st);
 
   std::cout << newSt.get() << std::endl;
-  std::cout << "Full or empty: " << newSt.isFull() << ' ' << newSt.isEmpty() << std::endl;
-
+  std::cout << "Full or empty: " << newSt.isFull() << ' ';
+  std::cout << newSt.isEmpty() << std::endl;
 
   while (!st.isEmpty()) {
     std::cout << st.pop() << ' ' << newSt.pop() << std::endl;
   }
 
-  std::cout << "Full or empty: " << st.isFull() << ' ' << st.isEmpty() << std::endl;
-  std::cout << "Full or empty: " << newSt.isFull() << ' ' << newSt.isEmpty() << std::endl;
+  std::cout << "Full or empty: " << st.isFull() << ' ';
+  std::cout << st.isEmpty() << std::endl;
+  std::cout << "Full or empty: " << newSt.isFull() << ' ';
+  std::cout << newSt.isEmpty() << std::endl;
 
   return 0;
 }
