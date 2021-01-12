798a410139246dd6877d25a6435b8b8766d35855
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2021-01-12 14:18:47 +0300

    main is done

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// Copyright 2020 Khoroshavina Ekaterina
+#include "MyStack.h"
+
+int main() {
+    MyStack<int> stack(5);
+    stack.push(50);
+    stack.push(1);
+    stack.push(21);
+    MyStack<int> new_stack(stack);
+    std::cout << new_stack.get() << std::endl;
+    std::cout << new_stack.isFull() << std::endl;
+    new_stack.push(3);
+    new_stack.push(10);
+    std::cout << new_stack.isFull() << std::endl;
+    while (!stack.isEmpty())
+        std::cout << stack.pop() << ' ';
+    std::cout << std::endl;
+    return 0;
+}
