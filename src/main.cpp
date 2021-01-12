bb5b50c39422b52387ab4d1da30a1ef765a3fbc1
Nik <50590478+stifeevn@users.noreply.github.com>
2021-01-13 00:22:34 +0500

    fix2

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main() {
     MyStack<int> stack(10);
     for (int i = 0; i < 10; i++) {
-        int value = rand_r() % 10;
+        int value = std::rand() % 10;
         std::cout << "Push " << value << std::endl;
         stack.push(value);
     }
