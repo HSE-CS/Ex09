46ae1009f7a181880ab30716f24cb357cec8c574
MalininDD <54958481+MalininDD@users.noreply.github.com>
2021-01-12 13:23:30 +0300

    Add files via upload

--- include/MyStack.h
+++ include/MyStack.h
@@ -8,49 +8,48 @@
 template <class T>
 class MyStack{
  private:
-    int size = 0;
-    int current = 0;
+    int size;
+    int current;
     T * mass;
 
  public:
-    explicit MyStack(int s = 10){
+    explicit MyStack(int s = 10) {
         size = s;
         mass = new T[size];
     }
-    
-    MyStack(const MyStack &other){//конструктор копирования
+
+    MyStack(const MyStack &other) {
         this->size = other->size;
         this->mass = new T[other->size];
         for (int i = 0; i < other->size; i++) {
             this->mass[i] = other->mass[i];
         }
     }
-    
-    ~MyStack(){
+
+    ~MyStack() {
         size = 0;
         current = 0;
         delete[] mass;
-
     }
     
     T get() const{
         if (!isEmpty()) {
-            return mass[0];
+            return mass[current-1];
         }
         else{
             throw "Can not get a stack item because the stack is empty";
         }
     }
     
-    T pop(){
+    T pop() {
         if (!isEmpty()) {
-            return mass[0];
+            return mass[--current];
         } else {
             throw "Can not get a stack item because the stack is empty";
         }
     }
     
-    void push(T a){
+    void push(T a) {
         if (!isFull()) {
             mass[current] = a;
             current++;
