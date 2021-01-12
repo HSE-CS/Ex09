e7f93b3c6226cd448cc5b8630d0e35c8994bd4f6
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2021-01-12 11:02:38 +0300

    complete work

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,8 @@
 #include <stdlib.h>
 #include "MyStack.h"
 int main() {
-  MyStack<int> st(10);
-  st.push(rand());
-  st.push(rand());
-  st.push(rand());
-
-  while (!st.isEmpty())
-      std::cout << st.pop() << " ";
   MyStack <int> myStack(5);
-
-  // заполняем стек
+  // Р·Р°РїРѕР»РЅСЏРµРј СЃС‚РµРє
   std::cout << "PUSH:\n";
   int ct = 0;
   while (ct++ != 5)
@@ -21,16 +13,58 @@ int main() {
     std::cin >> temp;
     myStack.push(temp);
   }
-
-  myStack.printStack(); // вывод стека на экран
-
+  myStack.printStack(); // РІС‹РІРѕРґ СЃС‚РµРєР° РЅР° СЌРєСЂР°РЅ
   std::cout << "\nDEL:\n";
-
-  myStack.pop(); // удаляем элемент из стека
-  myStack.pop(); // удаляем элемент из стека
-  myStack.printStack(); // вывод стека на экран
-	return 0;
+  myStack.pop(); // СѓРґР°Р»СЏРµРј СЌР»РµРјРµРЅС‚ РёР· СЃС‚РµРєР°
+  myStack.pop(); // СѓРґР°Р»СЏРµРј СЌР»РµРјРµРЅС‚ РёР· СЃС‚РµРєР°
+  myStack.printStack(); // РІС‹РІРѕРґ СЃС‚РµРєР° РЅР° СЌРєСЂР°РЅ
+  MyStack<int> st(10);
+  std::cout << st.isEmpty() << true << std::endl;
+  std::cout << "------------------------" << std::endl;
+  MyStack<int> myStack2(10);
+  myStack2.push(5);
+  std::cout << myStack2.isEmpty() << false << std::endl;
+  std::cout << "------------------------" << std::endl;
+  MyStack<int> myStack3(10);
+  myStack3.push(5);
+  myStack3.pop();
+  std::cout << myStack3.isEmpty() << true << std::endl;
+  std::cout << "------------------------" << std::endl;
+  MyStack<int> myStack4;
+  std::cout << myStack4.isEmpty() << true << std::endl;
+  std::cout << "------------------------" << std::endl;
+  MyStack<int> myStack5(10);
+  myStack5.push(5);
+  MyStack<int> myStack5_1 = myStack5;
+  std::cout << 5 <<  myStack5_1.get() << std::endl;
+  std::cout << "------------------------" << std::endl;
+  MyStack<int> myStack6(10);
+  myStack6.push(5);
+  myStack6.push(6);
+  myStack6.push(7);
+  std::cout << 7 << myStack6.get() << std::endl;
+  std::cout << "------------------------" << std::endl;
+  MyStack<int> myStack7(10);
+  myStack7.push(5);
+  myStack7.push(6);
+  myStack7.push(7);
+  myStack7.pop();
+  std::cout << 6 << myStack7.get() << std::endl;
+  std::cout << "------------------------" << std::endl;
+  MyStack<int> myStack8(3);
+  myStack8.push(5);
+  myStack8.push(6);
+  myStack8.push(7);
+  std::cout << true << myStack8.isFull() << std::endl;
+  std::cout << "------------------------" << std::endl;
+  MyStack<int> myStack9(3);
+  myStack9.push(5);
+  myStack9.push(6);
+  std::cout << false << myStack9.isFull() << std::endl;
+  std::cout << "------------------------" << std::endl;
+  MyStack<int> myStack10;
+  std::cout << NULL << myStack10.get() << std::endl;
+  std::cout << "------------------------" << std::endl;
+    return 0;
 }
-
-
 
