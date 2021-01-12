86864d153359bd41a29cfc2579ef8d74c2434f62
Dmitry <dimitri.vargin@yandex.ru>
2021-01-12 16:10:46 +0300

    init

---
+++ include/MyStack.h
@@ -0,0 +1,60 @@
+#ifndef EX09_MYSTACK_H
+#define EX09_MYSTACK_H
+
+#include "iostream"
+
+template <class T>
+class MyStack{
+private:
+    int max_length;
+    int length = 0;
+    T *arr;
+public:
+    MyStack(int max_length){
+        this->max_length = max_length;
+        this->arr = new T[max_length];
+    }
+
+    MyStack(const MyStack<T> &stack){
+        this->max_length = stack.max_length;
+        this->length = stack.length;
+        this->arr = new T[stack.max_length];
+        for (int i = 0; i < stack.max_length; ++i) {
+            this->arr[i] = stack.arr[i];
+        }
+    }
+
+    ~MyStack(){
+        delete[] this->arr;
+    }
+
+    T get() const{
+        return this->arr[this->length-1];
+    }
+
+    T pop() {
+        return this->arr[--this->length];
+    }
+
+    void push(T element){
+        this->arr[length++] = element;
+    }
+
+    bool isFull() const{
+        return this->length == this->max_length;
+    }
+
+    bool isEmpty() const{
+        return this->length == 0;
+    }
+
+    void printStack() const{
+        std::cout << "Stack:" << std::endl;
+        for (int i = 0; i < this->length; ++i) {
+            std::cout << this->arr[i] << ' ';
+        }
+        std::cout << "\n\n";
+    }
+};
+
+#endif //EX09_MYSTACK_H
---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include "../include/MyStack.h"
+using namespace std;
+
+int main(){
+    MyStack<int> stack(5);
+    stack.push(32);
+    stack.push(-5);
+    stack.push(1);
+    stack.push(0);
+    stack.push(-46354);
+    cout << "Create stack with length argument and push 2 elements:" << endl;
+    stack.printStack();
+
+    cout << "Get last element:" << endl;
+    cout << stack.get() << endl << endl;
+
+    cout << "Pop last element: " << endl;
+    cout << "removed element: " << stack.pop() << endl;
+    stack.printStack();
+
+    cout << "Copy constructor:" << endl;
+    MyStack<int> copy_stack(stack);
+    copy_stack.printStack();
+
+    cout << "Is it empty? - " << copy_stack.isEmpty() << endl;
+    cout << "Is it full? - " << copy_stack.isFull() << endl << endl;
+
+    cout << "After push 1 element" << endl;
+    copy_stack.push(10);
+    cout << "Is it empty? - " << copy_stack.isEmpty() << endl;
+    cout << "Is it full? - " << copy_stack.isFull() << endl << endl;
+
+    while (!copy_stack.isEmpty())
+        copy_stack.pop();
+    cout << "After push all elements" << endl;
+    cout << "Is it empty? - " << copy_stack.isEmpty() << endl;
+    cout << "Is it full? - " << copy_stack.isFull() << endl << endl;
+
+    return 0;
+}
