fa16dbd5bdf66a5c46524e564d3cfb3344f38ddd
Elizaveta Drobot <lizunchik7@bk.ru>
2021-01-12 16:31:17 +0300

    test3

--- test/tests.cpp
+++ test/tests.cpp
@@ -29,20 +29,16 @@ TEST(MyStackTest, test_4) {
 }
 
 TEST(MyStackTest, test_5) {
+	MyStack<int> st(1);
+	st.push(3);
+	EXPECT_EQ(3, st.get());
+}
+
+TEST(MyStackTest, test_6) {
 	MyStack<int> st(2);
 	st.push(3);
 	st.push(4);
 	EXPECT_EQ(4, st.get());
-}
-
-TEST(MyStackTest, test_6) {
-	MyStack<int> st(5);
-	st.push(3);
-	st.push(4);
-	st.push(2);
-	st.push(5);
-	st.pop();
-	EXPECT_EQ(2, st.get());
 }
 
 TEST(MyStackTest, test_7) {
@@ -69,17 +65,13 @@ TEST(MyStackTest, test_9) {
 	MyStack<int> st(5);
 	st.push(3);
 	st.push(4);
-	st.push(2);
-	st.push(5);
 	st.pop();
-	st.pop();
-	EXPECT_EQ(4, st.get());
+	EXPECT_EQ(3, st.get());
 }
 
 TEST(MyStackTest, test_10) {
 	MyStack<int> st(5);
 	st.push(3);
 	st.push(4);
-	st.pop();
-	EXPECT_EQ(3, st.get());
+	EXPECT_EQ(4, st.get());
 }
