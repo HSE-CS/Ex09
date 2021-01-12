161c06f8ed32910129fdc473c4bd6507dc8bd622
danek0100 <71225718+danek0100@users.noreply.github.com>
2021-01-12 13:22:16 +0300

    rand_r

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 // Copyright 2020 <Jiiijyyy>
 #include "MyStack.h"
 #include <iostream>
+#include <ctime>
 
 int main() {
+    unsigned int seed = time(NULL);
     MyStack<int> st(10);
-    st.push(rand_r());
-    st.push(rand_r());
-    st.push(rand_r());
+    st.push(rand_r(&seed));
+    st.push(rand_r(&seed));
+    st.push(rand_r(&seed));
 
     while (!st.isEmpty())
         std::cout << st.pop() << " ";
