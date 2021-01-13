29bd5f55a837bd99c9eec2905eca2c0fabbe8553
ch2oh-ch2oh <valerasofronov01@gmail.com>
2021-01-13 03:49:39 +0300

    header

---
+++ include/MyStack.h
@@ -0,0 +1,46 @@
+// Copyright 2021 ch2oh
+
+#include <cstddef>
+#include <vector>
+
+template<typename T>
+class MyStack {
+private:
+    std::vector<T> *stack;
+    size_t size;
+public:
+    explicit MyStack(size_t t) : stack(new std::vector<T>()), size(t) {}
+
+    MyStack(const MyStack<T> &toCopy) : size(toCopy.size) {
+        stack = new std::vector<T>();
+        for (T t : *toCopy.stack) stack->push_back(t);
+    }
+
+    ~MyStack() {
+        delete stack;
+    }
+
+    T get() const {
+        return stack->back();
+    }
+
+    T pop() {
+        if (!isEmpty()) {
+            T t = get();
+            stack->pop_back();
+            return t;
+        }
+    }
+
+    void push(T x) {
+        if (!isFull()) stack->push_back(x);
+    }
+
+    bool isFull() const {
+        return stack->size() == size;
+    }
+
+    bool isEmpty() const {
+        return stack->empty();
+    }
+};
