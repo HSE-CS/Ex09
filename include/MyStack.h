cd77b5317b2d3c5d758b1c69acdd262077f8c00e
ch2oh-ch2oh <valerasofronov01@gmail.com>
2021-01-13 03:58:07 +0300

    header fix

--- include/MyStack.h
+++ include/MyStack.h
@@ -11,8 +11,8 @@ class MyStack {
  private:
      std::vector<T> *stack;
      size_t size;
+     
  public:
-
     explicit MyStack(size_t t) : stack(new std::vector<T>()), size(t) {}
 
     MyStack(const MyStack<T> &toCopy) : size(toCopy.size) {
