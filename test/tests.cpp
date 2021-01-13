29f45a3d514d33af03a52f367b768fc871181207
Andrej Kostin <andrej_kostin_2001@mail.ru>
2021-01-13 03:42:06 +0300

    fix 2

--- test/tests.cpp
+++ test/tests.cpp
@@ -11,79 +11,79 @@ TEST(MyStackTest, test1) {
 }
 
 TEST(MyStackTest, test2) {
-MyStack<int> st(5);
-st.push(400);
-EXPECT_EQ(false, st.isEmpty());
+  MyStack<int> st(5);
+  st.push(400);
+  EXPECT_EQ(false, st.isEmpty());
 }
 
 TEST(MyStackTest, test3) {
-MyStack<int> st(1);
-st.push(400);
-st.push(400);
-EXPECT_EQ(true, st.isFull());
+  MyStack<int> st(1);
+  st.push(400);
+  st.push(400);
+  EXPECT_EQ(true, st.isFull());
 }
 
 TEST(MyStackTest, test4) {
-MyStack<int> st(5);
-EXPECT_EQ(false, st.isFull());
+  MyStack<int> st(5);
+  EXPECT_EQ(false, st.isFull());
 }
 
 TEST(MyStackTest, test5) {
-MyStack<int> st(5);
-st.push(228);
-EXPECT_EQ(228, st.get());
+  MyStack<int> st(5);
+  st.push(228);
+  EXPECT_EQ(228, st.get());
 }
 
 TEST(MyStackTest, test6) {
-MyStack<int> st(5);
-st.push(228);
-EXPECT_EQ(228, st.pop());
+  MyStack<int> st(5);
+  st.push(228);
+  EXPECT_EQ(228, st.pop());
 }
 
 TEST(MyStackTest, test7) {
-MyStack<int> st(5);
-st.push(228);
-st.pop()
-EXPECT_EQ(true, st.isEmpty());
+  MyStack<int> st(5);
+  st.push(228);
+  st.pop()
+  EXPECT_EQ(true, st.isEmpty());
 }
 
 TEST(MyStackTest, test8) {
-MyStack<int> st(5);
-st.push(228);
-st.get()
-EXPECT_EQ(false, st.isEmpty());
+  MyStack<int> st(5);
+  st.push(228);
+  st.get()
+  EXPECT_EQ(false, st.isEmpty());
 }
 
 TEST(MyStackTest, test9) {
-MyStack<int> st(5);
-st.push(228);
-st.push(228);
-st.push(228);
-st.push(228);
-st.push(228);
-st.push(228);
-int summa=0;
-while(!st.isEmpty())
-summa+=st.pop()
-EXPECT_EQ(1368, summa);
+  MyStack<int> st(5);
+  st.push(228);
+  st.push(228);
+  st.push(228);
+  st.push(228);
+  st.push(228);
+  st.push(228);
+  int summa=0;
+  while(!st.isEmpty())
+      summa+=st.pop()
+  EXPECT_EQ(1368, summa);
 }
 
 TEST(MyStackTest, test10) {
-MyStack<int> st(5);
-st.push(1);
-st.push(2);
-st.get();
-st.pop();
-EXPECT_EQ(1, st.get());
+  MyStack<int> st(5);
+  st.push(1);
+  st.push(2);
+  st.get();
+  st.pop();
+  EXPECT_EQ(1, st.get());
 }
 
 TEST(MyStackTest, test11) {
-MyStack<int> st(5);
-st.push(1);
-st.push(2);
-st.get();
-st.pop();
-st.push(3);
-st.pop();
-EXPECT_EQ(1, st.get());
+  MyStack<int> st(5);
+  st.push(1);
+  st.push(2);
+  st.get();
+  st.pop();
+  st.push(3);
+  st.pop();
+  EXPECT_EQ(1, st.get());
 }
