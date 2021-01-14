343555b998d80db59cc15e7dfb2be4145e179ddd
Olesya Nikolaeva <63335416+olesyani@users.noreply.github.com>
2021-01-14 18:39:43 +0300

    first try

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+// Copyright [2020] <Olesya Nikolaeva>
+
+#include <iostream>
+#include "MyStack.h"
+
+int main() {
+    MyStack<int> st(10);
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    while(st.isEmpty() == 0) {
+        std::cout << st.pop() << " ";
+    }
+    
+    return 0;
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,11 +1,75 @@
-// Copyright 2020 GHA Test Team
+// Copyright [2020] <Olesya Nikolaeva>
+
 #include "MyStack.h"
 
 #include <gtest/gtest.h>
-#include <string>
 
 TEST(MyStackTest, test1) {
-  MyStack<int> st(5);
-  EXPECT_EQ(true, st.isEmpty());
+    MyStack<int> stack(10);
+    stack.push(1);
+    EXPECT_EQ(1, stack.get());
 }
 
+TEST(MyStackTest, test2) {
+    MyStack<int> stack(10);
+    stack.push(1);
+    stack.push(2);
+    EXPECT_EQ(2, stack.get());
+}
+
+TEST(MyStackTest, test3) {
+    MyStack<int> stack(10);
+    stack.push(1);
+    stack.push(2);
+    EXPECT_EQ(1, stack.pop());
+}
+
+TEST(MyStackTest, test4) {
+    MyStack<int> stack(10);
+    stack.push(2);
+    stack.push(4);
+    stack.push(6);
+    EXPECT_EQ(4, stack.pop());
+}
+
+TEST(MyStackTest, test5) {
+    MyStack<int> stack(10);
+    EXPECT_EQ(true, stack.isEmpty());
+}
+
+TEST(MyStackTest, test6) {
+    MyStack<int> stack(10);
+    EXPECT_EQ(false, stack.isFull());
+}
+
+TEST(MyStackTest, test7) {
+    MyStack<int> stack(10);
+    stack.push(2);
+    EXPECT_EQ(false, stack.isFull());
+}
+
+TEST(MyStackTest, test8) {
+    MyStack<int> stack(10);
+    stack.push(1);
+    stack.push(2);
+    stack.pop();
+    EXPECT_EQ(1, stack.get());
+}
+
+TEST(MyStackTest, test9) {
+    MyStack<int> stack(10);
+    stack.push(2);
+    stack.push(4);
+    stack.push(6);
+    stack.pop();
+    EXPECT_EQ(2, stack.pop());
+}
+
+TEST(MyStackTest, test10) {
+    MyStack<char> stack(10);
+    stack.push('a');
+    stack.push('b');
+    stack.push('c');
+    stack.pop();
+    EXPECT_EQ('a', stack.pop());
+}
