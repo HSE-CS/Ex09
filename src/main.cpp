ea1ed33aba629e63a0d65771d9e01d96c3d50084
Olesya Nikolaeva <63335416+olesyani@users.noreply.github.com>
2021-01-14 18:51:02 +0300

    main updated

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,8 @@ int main() {
     st.push(1);
     st.push(2);
     st.push(3);
-    while(st.isEmpty() == 0) {
+    while (st.isEmpty() == 0) {
         std::cout << st.pop() << " ";
     }
-    
     return 0;
 }
