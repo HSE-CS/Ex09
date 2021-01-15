ef2311629f24690d9854a03677b640a896202914
Ksuvot <ksuvot36@yandex.ru>
2021-01-15 21:41:16 +0300

    task 3

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,3 +9,76 @@ TEST(MyStackTest, test1) {
   EXPECT_EQ(true, st.isEmpty());
 }
 
+TEST(MyStackTest, test2) {
+  MyStack<int> st(2);
+  st.push(6);
+  st.push(101);
+  EXPECT_EQ(101, st.get());
+}
+
+TEST(MyStackTest, test3) {
+  MyStack<int> st(5);
+  st.push(0);
+  st.push(1);
+  st.push(2);
+  st.push(3);
+  st.push(4);
+  EXPECT_EQ(true, st.isFull());
+}
+
+TEST(MyStackTest, test4) {
+  MyStack<int> st(5);
+  st.push(0);
+  st.push(1);
+  st.push(2);
+  st.push(3);
+  st.push(4);
+  EXPECT_EQ(false, st.isEmpty());
+}
+
+TEST(MyStackTest, test5) {
+MyStack<int> st(6);
+st.push(17);
+st.push(15);
+st.push(14);
+st.push(12);
+st.pop();
+EXPECT_EQ(14, st.pop());
+}
+
+TEST(MyStackTest, test6) {
+MyStack<int> st(3);
+st.push(1);
+st.push(15);
+st.push(30);
+st.pop();
+EXPECT_EQ(15, st.get());
+}
+
+TEST(MyStackTest, test7) {
+MyStack<int> st(7);
+MyStack<int> st_1(st);
+EXPECT_EQ(st_1.pop(), 0);
+}
+
+TEST(MyStackTest, test8) {
+MyStack<int> st(8);
+st.push(8);
+st.get();
+EXPECT_EQ(false, st.isEmpty());
+}
+
+TEST(MyStackTest, test9) {
+MyStack<int> st(9);
+EXPECT_EQ(-1, st.pop());
+}
+
+TEST(MyStackTest, test10) {
+MyStack<int> st(10);
+st.push(11);
+st.push(12);
+st.push(13);
+st.push(14);
+st.pop();
+EXPECT_EQ(13, st.get());
+}
