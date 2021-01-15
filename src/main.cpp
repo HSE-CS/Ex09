8ad784d5506cd6938324ca426a1ac7db99bfef83
Ksuvot <ksuvot36@yandex.ru>
2021-01-15 21:25:44 +0300

    task 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 //  Copyright © 2021 Ksuvot. All rights reserved.
 #include "MyStack.h"
-/*
+
 int main() {
+  MyStack<int> st(4);
+  st.push(7);
+  st.push(9);
+  st.push(123);
+
+  MyStack<int> st_1(st);
+  while (!st_1.isEmpty())
+    std::cout << st_1.pop() <<" ";
+
+  std::cout << st.get() << std::endl;
+  std::cout << st.isFull() << std::endl;
+  std::cout << st.isEmpty() << std::endl;
 
   return 0;
-}*/
+}
