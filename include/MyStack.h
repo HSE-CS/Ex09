3786ac797594a1d28b3925dfa9e7eb64229b3589
Ksuvot <ksuvot36@yandex.ru>
2021-01-15 23:07:43 +0300

    fix 1.1

--- include/MyStack.h
+++ include/MyStack.h
@@ -5,69 +5,50 @@
 
 #include <iostream>
 
-template <typename T>
-class MyStack {
+template <class T>
+class MyStack{
  private:
-  T* ptr;
+  T *ptr;
   int size;
-  int index;
+  int index = 0;
 
  public:
-  MyStack() {
-    this->size = 0;
-    this->index = -1;
-    this->ptr = nullptr;
-  }
-
-  explicit MyStack(int size) : index(-1) {
+  explicit MyStack(int size) {
     this->size = size;
     this->ptr = new T[size];
   }
 
-  MyStack(const MyStack& my_stack) {
-    this->index = my_stack.index;
+  MyStack(const MyStack<T> &my_stack) {
     this->size = my_stack.size;
+    this->index = my_stack.index;
     this->ptr = new T[my_stack.size];
-    for (unsigned i = 0; i < my_stack.size; i++)
+    for (int i = 0; i < my_stack.size; i++) {
       this->ptr[i] = my_stack.ptr[i];
-  }
-
-  ~MyStack() {
-    if (this->size > 0) {
-      delete[] ptr;
-      this->size = 0;
-      this->index = -1;
     }
   }
 
-  T get()const {
-    if (!this->isEmpty())
-      return this->ptr[this->index];
-    else
-      throw "ERROR";
+  ~MyStack() {
+    delete[] this->ptr;
+  }
+
+  T get() const {
+    return this->ptr[this->index-1];
   }
 
   T pop() {
-    if (this->isEmpty())
-      throw "ERROR";
-    T temp = this->ptr[this->size];
-    --this->index;
-    return temp;
+    return this->ptr[--this->index];
   }
 
-  void push(const T key) {
-    if (this->isEmpty())
-      throw "ERROR";
-    else
-      this->ptr[++this->index] = key;
+  void push(T key) {
+    this->ptr[index++] = key;
   }
 
   bool isFull() const {
-    return this->index == this->size - 1;
+    return this->index == this->size;
   }
 
   bool isEmpty() const {
-    return this->index == -1;
+    return this->index == 0;
   }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,30 @@
 #include "MyStack.h"
 
 int main() {
-  MyStack<int> st(4);
-  st.push(7);
-  st.push(9);
-  st.push(123);
+  MyStack<int> st(5);
+  st.push(1);
+  st.push(2);
+  st.push(3);
+  st.push(4);
+  st.push(5);
 
-  MyStack<int> st_1(st);
-  while (!st_1.isEmpty())
-    std::cout << st_1.pop() <<" ";
-
-  std::cout << st.get() << std::endl;
-  std::cout << st.isFull() << std::endl;
   std::cout << st.isEmpty() << std::endl;
+  std::cout << st.isFull() << std::endl;
+  std::cout << st.get() << std::endl;
+  std::cout << st.pop() << std::endl;
+
+  MyStack<int> st_copy(st);
+  std::cout << st_copy.isEmpty() << std::endl;
+  std::cout << st_copy.isFull() << std::endl;
+  st_copy.push(6);
+  std::cout << st_copy.isEmpty() << std::endl;
+  std::cout << st_copy.isFull() << std::endl;
+
+  while (!st_copy.isEmpty())
+    st_copy.pop();
+
+  std::cout << st_copy.isEmpty() << std::endl;
+  std::cout << st_copy.isFull() << std::endl;
 
   return 0;
 }
