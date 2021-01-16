3cbf68c7585fbdb8e6fe7f96f7bf7c76182d4b0d
Right-bugor <71226571+Right-bugor@users.noreply.github.com>
2021-01-16 22:36:58 +0300

    first

--- include/MyStack.h
+++ include/MyStack.h
@@ -5,18 +5,18 @@
 template<class T>
 class MyStack {
 private:
-	T* Mystack;
-	int len;
-	int top;
+    T* Mystack;
+    int len;
+    int top;
 public:
-	explicit MyStack(int);
-	MyStack(const MyStack&);
-	~MyStack();
-	T get() const;
-	void push(T);
-	T pop();
-	bool isFull() const;
-	bool isEmpty() const;
+    explicit MyStack(int);
+    MyStack(const MyStack&);
+    ~MyStack();
+    T get() const;
+    void push(T);
+    T pop();
+    bool isFull() const;
+    bool isEmpty() const;
 };
 
 template<class T>
@@ -24,57 +24,57 @@ MyStack<T>::MyStack(int _len) : top(-1), len(_len), Mystack(new T[len]) {}
 
 template<class T>
 MyStack<T>::MyStack(const MyStack& another_Mystack) {
-	top = another_Mystack.top;
-	Mystack = new T[another_Mystack.len];
-	len = another_Mystack.len;
-	for (int i = 0; i < len; ++i) {
-		Mystack[i] = another_Mystack.Mystack[i];
-	}
+    top = another_Mystack.top;
+    Mystack = new T[another_Mystack.len];
+    len = another_Mystack.len;
+    for (int i = 0; i < len; ++i) {
+        Mystack[i] = another_Mystack.Mystack[i];
+    }
 }
 
 template<class T>
 MyStack<T>::~MyStack() {
-	delete[] Mystack;
+    delete[] Mystack;
 }
 
 template<class T>
 bool MyStack<T>::isEmpty() const {
-	return top == -1;
+    return top == -1;
 }
 
 template<class T>
 bool MyStack<T>::isFull() const {
-	return top == len - 1;
+    return top == len - 1;
 }
 
 template<class T>
 T MyStack<T>::get() const {
-	if (!isEmpty()) {
-		return Mystack[top];
-	}
-	else {
-		throw "The Mystack is empty";
-	}
+    if (!isEmpty()) {
+        return Mystack[top];
+    }
+    else {
+        throw "The Mystack is empty";
+    }
 }
 
 template<class T>
 void MyStack<T>::push(T x) {
-	if (isFull()) {
-		throw "The Mystack is full";
-	}
-	else {
-		Mystack[++top] = x;
-	}
+    if (isFull()) {
+        throw "The Mystack is full";
+    }
+    else {
+        Mystack[++top] = x;
+    }
 }
 
 template<class T>
 T MyStack<T>::pop() {
-	if (isEmpty()) {
-		throw "The Mystack is empty";
-	}
-	else {
-		return Mystack[top--];
-	}
+    if (isEmpty()) {
+        throw "The Mystack is empty";
+    }
+    else {
+        return Mystack[top--];
+    }
 }
 
 #endif  // INCLUDE_MYSTACK_H_
--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,16 @@
 #include "MyStack.h"
 
 int main() {
-	MyStack<int> st(10);
-	std::cout << st.isEmpty() << '\n';
-	st.push(1);
-	std::cout << st.get() << '\n';
-	st.push(1);
-	st.push(1);
-	std::cout << st.isFull() << '\n';
-	st.push(4);
-	std::cout << st.isFull() << '\n';
-	while (!st.isEmpty())
-		std::cout << st.pop() << " ";
-	return 0;
+    MyStack<int> st(10);
+    std::cout << st.isEmpty() << '\n';
+    st.push(1);
+    std::cout << st.get() << '\n';
+    st.push(1);
+    st.push(1);
+    std::cout << st.isFull() << '\n';
+    st.push(4);
+    std::cout << st.isFull() << '\n';
+    while (!st.isEmpty())
+        std::cout << st.pop() << " ";
+    return 0;
 }
