65cac8e405e2f24de6eb8c50ce2f49753ef6d5b2
Irina <pasmanikirina29@gmail.com>
2021-01-18 17:12:42 +0300

    text fixes

--- include/MyStack.h
+++ include/MyStack.h
@@ -5,53 +5,54 @@
 template <typename T>
 
 class MyStack {
-private:
-	unsigned int size;
-	unsigned int head = 0;
-	T* value;
+private: 
+    unsigned int size;
+    unsigned int head = 0;
+    T* value;
 
 public:
 
-	explicit MyStack(unsigned int new_size) {	 //конструктор с одним параметром
-		size = new_size;
-		value = new T[size];
-	}
+    explicit MyStack(unsigned int new_size) {	 //конструктор с одним параметром
+        size = new_size;
+        value = new T[size];
+    }
 
-	MyStack(const MyStack& tmp) {	 //конструктор копирования
-		size = tmp.size;
-		head = tmp.head;
-		value = new T[size];
-		for (unsigned int i = 0; i < head; ++i)
-			value[i] = tmp.value[i];
-	}
-	~MyStack() {	 //деструктор
-		delete value;
-	}
+    MyStack(const MyStack& tmp) {	 //конструктор копирования
+        size = tmp.size;
+        head = tmp.head;
+        value = new T[size];
+        for (unsigned int i = 0; i < head; ++i)
+            value[i] = tmp.value[i];
+    }
 
-	T get() const {	  //возвращение элемента на вершине стека(без изменения состояния последнего)
-		return value[head - 1];
-	}
+    ~MyStack() {	 //деструктор
+        delete value;
+    }
 
-	T pop() {	 //выталкивание элемента из стека
-		head--;
-		return value[head];
-	}
+    T get() const {	  //возвращение элемента на вершине стека(без изменения состояния последнего)
+        return value[head - 1];
+    }
 
-	void push(T num) {	 //добавление элемента в стек
-		value[head] = num;
-		head++;
-	}
+    T pop() {	 //выталкивание элемента из стека
+        head--;
+        return value[head];
+    }
 
-	bool isFull() const {	 //проверка заполненности стека
-		if (head == size) return true;
-		else return false;
-	}
+    void push(T num) {	 //добавление элемента в стек
+        value[head] = num;
+        head++;
+    }
 
-	bool isEmpty() const {	 //проверка пустоты стека
-		if (head == 0) return true;
-		else return false;
-	}
+    bool isFull() const {	 //проверка заполненности стека
+        if (head == size) return true;
+        else return false;
+    }
+
+    bool isEmpty() const {	 //проверка пустоты стека
+        if (head == 0) return true;
+        else return false;
+    }
 
 };
 
-#endif
+#endif  // INCLUDE_MYSTACK_H_
