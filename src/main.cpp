8dd021e130d11f767ab9699226a0ab1af338957f
Irina <pasmanikirina29@gmail.com>
2021-01-18 18:32:31 +0300

    main added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 by idpas
+#include "MyStack.h"
+#include <iostream>
 
 int main() {
+    MyStack<int> st(5);
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
 
-	return 0;
+    std::cout << st.isFull() << std::endl;
+
+    std::cout << st.isEmpty() << std::endl;
+
+    std::cout << st.get() << "    ";
+    st.pop();
+    std::cout << st.get() << std::endl;
+
+    std::cout << st.get() << "    ";
+    st.push(6);
+    st.push(7);
+    std::cout << st.get() << std::endl;
+
+    return 0;
 }
