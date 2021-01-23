cc79567e4982780754ed1d2385e28b79ad710f26
Ramazan <ramazankka220@gmail.com>
2021-01-23 23:31:49 +0500

    Uf

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+//Copyright Salakhov Ramazan
+#include "MyStack.h"
+
+using namespace std;
+int main()
+{
+    MyStack<char> st(5);
+    st.push('o');
+    st.push('l');
+    st.push('l');
+    st.push('e');
+    st.push('h');
+    MyStack <char> st2(st);
+
+
+    while (!st2.isEmpty())
+        cout << st2.pop() << " ";
+    return 0;
+}
