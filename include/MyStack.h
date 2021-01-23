3b804c08f13ee967d1b94d745f9790efd0c741a8
Ramazan <ramazankka220@gmail.com>
2021-01-23 23:52:53 +0500

    Ufffffffff

--- include/MyStack.h
+++ include/MyStack.h
@@ -3,20 +3,20 @@
 #pragma once
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
 
 template <typename T>
-class MyStack
-{
+class MyStack {
 private:
-    T* stackPtr; // указатель на стек
-    int size; // размер стека
-    T top = 0; // вершина стека
+     T* stackPtr;  // указатель на стек
+     int size;  // размер стека
+     T top = 0;  // вершина стека
 public:
     explicit MyStack(int n_size) {
         size = n_size;
         stackPtr = new T[size];
-    };
+    }
 
     MyStack(const MyStack& another_stack) {
         size = another_stack.size;
@@ -25,25 +25,25 @@ public:
 
         for (unsigned int i = 0; i < top; i = i + 1)
             stackPtr[i] = another_stack.stackPtr[i];
-    };
+    }
 
     ~MyStack() {
         delete[] stackPtr;
-    }; // деструктор
+    }  // деструктор
 
     void push(T value) {
         stackPtr[top] = value;
         top++;
-    }; // поместить элемент в стек
+    }  // поместить элемент в стек
 
     T pop() {
         top--;
         return stackPtr[top];
-    }; // удалить из стека элемент
+    }  // удалить из стека элемент
 
     T get() const {
         return stackPtr[top - 1];
-    };
+    }
 
     bool isFull() const {
         if (top == size) {
@@ -52,7 +52,7 @@ public:
         else {
             return false;
         };
-    };
+    }
 
     bool isEmpty() const {
         if (top == 0) {
@@ -61,5 +61,5 @@ public:
         else {
             return false;
         };
-    };
+    }
 };
