c285b6b0fd3e8ca76e0795ab1cb1edf655321ab1
eakrayushkina57 <eakrayushkina_1@edu.hse.ru>
2021-02-28 20:35:19 +0300

    b103

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,11 +1,11 @@
 // Copyright 2020 Краюшкина Екатерина
 
-/*#include "Stack.h"
+/*#include "MyStack.h"
 #include <gtest/gtest.h>
 #include <string>
 
-TEST(StackTest, test1) {	
-  Stack<int> st(5);	  	  
+TEST(MyStackTest, test1) {	
+  MyStack<int> st(5);	  	  
   st.push('3');
   st.push('4');
   st.push('5');
@@ -15,8 +15,8 @@ TEST(StackTest, test1) {
   st.pop();
   EXPECT_EQ('5', st.get()); }
 
-TEST(StackTest, test2) {
-  Stack<int> st(5);
+TEST(MyStackTest, test2) {
+  MyStack<int> st(5);
   st.push(1);
   st.push(2);
   st.push(3);
@@ -24,8 +24,8 @@ TEST(StackTest, test2) {
   st.push(5);
   EXPECT_EQ(5, st.get()); }
 
-TEST(StackTest, test3) {
-  Stack<int> st(5);
+TEST(MyStackTest, test3) {
+  MyStack<int> st(5);
   st.push(1);
   st.push(2);
   st.push(3);
@@ -34,8 +34,8 @@ TEST(StackTest, test3) {
   st.pop();
   EXPECT_EQ(4, st.pop()); }
 
-TEST(StackTest, test4) {
- Stack<int> st(5);
+TEST(MyStackTest, test4) {
+ MyStack<int> st(5);
  st.push('5');
  st.push('6');
  st.push('7');
@@ -45,16 +45,16 @@ TEST(StackTest, test4) {
  st.pop();
  EXPECT_EQ('7', st.pop()); }
 
-TEST(StackTest, test5) {
-  Stack<int> st(5);
+TEST(MyStackTest, test5) {
+  MyStack<int> st(5);
   EXPECT_EQ(1, st.isEmpty()); }
 
-TEST(StackTest, test6) {
-  Stack<int> st(5);
+TEST(MyStackTest, test6) {
+  MyStack<int> st(5);
   EXPECT_EQ(1, st.isEmpty()); }
 
-TEST(StackTest, test6) {
-  Stack<int> st(5);
+TEST(MyStackTest, test6) {
+  MyStack<int> st(5);
   st.push(1);
   st.push(2);
   st.push(3);
@@ -62,8 +62,8 @@ TEST(StackTest, test6) {
   st.push(5);
   EXPECT_EQ(0, st.isEmpty()); }
 
-TEST(StackTest, test7) {
-  Stack<int> st(5);
+TEST(MyStackTest, test7) {
+  MyStack<int> st(5);
   st.push(5);
   st.push(4);
   st.push(3);
@@ -71,14 +71,14 @@ TEST(StackTest, test7) {
   st.push(1);
   EXPECT_EQ(1, st.isFull()); }
 
-TEST(StackTest, test8) {
-  Stack<int> st(5);
+TEST(MyStackTest, test8) {
+  MyStack<int> st(5);
   EXPECT_EQ(1, st.isEmpty()); }
 
-TEST(StackTest, test9) {
-  Stack<int> st(5);
+TEST(MyStackTest, test9) {
+  MyStack<int> st(5);
   EXPECT_EQ(1, st.isEmpty()); }
 
-TEST(StackTest, test10) {
-  Stack<int> st(5);
+TEST(MyStackTest, test10) {
+  MyStack<int> st(5);
   EXPECT_EQ(1, st.isEmpty()); }*/
