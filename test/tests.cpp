3048ec9a1026ca877c7f6c20cbdfe60383e7cdae
Liza Birina <esbirina@edu.hse.ru>
2021-03-15 19:29:08 +0300

    popitka 2

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,32 +1,35 @@
 #include "MyStack.h"
 #include <gtest/gtest.h>
+
+
 TEST(MyStack, test1) 
 {
 	MyStack<int> st(3);
-	EXPECT_EQ(false, st.isEmpty());
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	EXPECT_EQ(true, st.isEmpty());
 }
 
 TEST(MyStack, test2) 
 {
 	MyStack<int> st(3);
-	st.push(2);
+	st.push(1);
+	st.push(3);
 	EXPECT_EQ(false, st.isEmpty());
 }
 
 TEST(MyStack, test3) 
 {
 	MyStack<int> st(3);
-	st.push(1);
-	st.push(2);
-	st.push(3);
-	EXPECT_EQ(false, st.isFull());
+
+	EXPECT_EQ(true, st.isFull());
 }
 
 TEST(MyStack, test4) 
 {
 	MyStack<int> st(3);
 	st.push(1);
-	st.push(2);
 	EXPECT_EQ(false, st.isFull());
 }
 
@@ -34,7 +37,7 @@ TEST(MyStack, test5)
 {
 	MyStack<int> st(3);
 	st.push(3);
-	EXPECT_EQ(1, st.get());
+	EXPECT_EQ(3, st.get());
 }
 
 TEST(MyStack, test6) 
@@ -45,7 +48,7 @@ TEST(MyStack, test6)
 	EXPECT_EQ(2, st.get());
 }
 
-TEST(MyStack, test7) 
+TEST(MyStack, test7)
 {
 	MyStack<int> st(3);
 	st.push(1);
@@ -54,22 +57,24 @@ TEST(MyStack, test7)
 	EXPECT_EQ(1, st.get());
 }
 
-TEST(MyStack, test8) {
+TEST(MyStack, test8) 
+{
 	MyStack<int> st(5);
 	st.push(1);
 	st.push(2);
+	st.push(3);
 	st.pop();
 	st.pop();
-	EXPECT_EQ(true, st.isEmpty());
+	EXPECT_EQ(1, st.isEmpty());
 }
 
 TEST(MyStack, test9) 
 {
 	MyStack<int> st(5);
 	st.push(1);
-	st.push(3);
+	st.push(2);
 	st.pop();
-	EXPECT_EQ(true, st.isFull());
+	EXPECT_EQ(false, st.isFull());
 }
 
 TEST(MyStack, test10) 
@@ -78,5 +83,6 @@ TEST(MyStack, test10)
 	st.push(1);
 	st.push(2);
 	st.pop();
-	EXPECT_EQ(false, st.get());
+	st.push(1234);
+	EXPECT_EQ(1234, st.get());
 }
