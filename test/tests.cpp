69770e21918ba674fff7ac3783c2b3df88d35342
skprpi <60091575+skprpi@users.noreply.github.com>
2021-03-17 11:06:14 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -39,7 +39,7 @@ TEST(MyStackTest, test6) {
     MyStack<int> st(2);
     st.push(10);
     st.push(12);
-    st.pop()
+    st.pop();
     EXPECT_EQ(false, st.isFull());
 }
 
@@ -47,7 +47,7 @@ TEST(MyStackTest, test7) {
     MyStack<int> st(20);
     st.push(10);
     st.push(12);
-    st.pop()
+    st.pop();
     EXPECT_EQ(12, st.pop());
     EXPECT_EQ(10, st.pop());
     EXPECT_EQ(true, st.isEmpty());
@@ -56,9 +56,9 @@ TEST(MyStackTest, test7) {
 TEST(MyStackTest, test8) {
     MyStack<int> st(20);
     st.push(10);
-    st.pop()
+    st.pop();
     st.push(12);
-    st.pop()
+    st.pop();
     EXPECT_EQ(true, st.isEmpty());
 }
 
@@ -66,8 +66,8 @@ TEST(MyStackTest, test9) {
     MyStack<int> st(20);
     st.push(10);
     st.push(12);
-    st.pop()
-    st.pop()
+    st.pop();
+    st.pop();
     EXPECT_EQ(true, st.isEmpty());
 }
 
@@ -77,8 +77,8 @@ TEST(MyStackTest, test10) {
     st.push(12);
     st.push(13);
     st.push(14);
-    st.pop()
-    st.pop()
+    st.pop();
+    st.pop();
     EXPECT_EQ(false, st.isEmpty());
     EXPECT_EQ(12, st.get());
     EXPECT_EQ(12, st.get());
