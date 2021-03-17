9f2806a3ed19dfcd4672ba0df877c7afd89d56d8
skprpi <60091575+skprpi@users.noreply.github.com>
2021-03-17 10:29:30 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MyStack.h"
+
+
+int main() {
+    MyStack<int> st(10);
+    st.push(rand());
+    st.push(rand());
+    st.push(rand());
+
+    while (!st.isEmpty())
+        std::cout << st.pop() << " ";
+}
