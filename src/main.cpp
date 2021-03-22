582f92c0205c753e1ba61ad54e91dbde9296675f
6a16ec <semaev2014@yandex.ru>
2021-03-22 16:45:33 +0300

    Improved version of using the class

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,16 @@ using namespace std;
 
 
 int main(){
-	MyStack<int> st(10);
-	st.push(1);
-//	st.push(10);
-//	st.push(rand());
-//	st.push(rand());
-//	st.push(rand());
-
-	while(!st.isEmpty())
-	   std::cout << st.pop() <<" ";
-//	string str = "(()((())))((()()))()";
-//	MyStack <char> stack(str.size());
-//	cout << "ok2";
-//	for(auto c: str){
-//		if(c == '('){
-//			stack.push(c);
-//		}
-//		else{
-//			stack.pop();
-//		}
-//		cout << c << endl;
-//	}
-
-	// cout << stack.isEmpty() ? "YES" : "NO";
-
-	cout << "end of program";
+	string str = "(()((())))((()()))()";
+	MyStack <char> stack(str.size());
+	for(auto c: str){
+		if(c == '('){
+			stack.push(c);
+		}
+		else{
+			stack.pop();
+		}
+	}
+	cout << (stack.isEmpty() ? "YES" : "NO");
 	return 0;
 }
