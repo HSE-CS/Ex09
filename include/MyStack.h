c15bc28969d1012f6b04d8e26506919ad39e831a
burgundypie <kotyonok2001@mai.ru>
2021-03-23 19:09:40 +0300

    Симонова Арина

---
+++ include/MyStack.h
@@ -0,0 +1,75 @@
+#ifndef INCLUDE_MYSTACK_H_
+#define INCLUDE_MYSTACK_H_
+
+#include <iostream>
+#include <cstdlib>
+
+template <typename T>
+class MyStack {
+	T* values;
+	size_t size;
+	size_t top;
+public:
+	explicit MyStack(size_t size);
+	MyStack(const MyStack& stack);
+	T get() const;
+	void pop();
+	void push(const T& value);
+	bool isFull() const;
+	bool isEmpty() const;
+	~MyStack();
+};
+
+template <typename T>
+MyStack<T>::MyStack(size_t size) {
+	this->size = size;
+	this->top = 0;
+	this->values = new T[size];
+}
+
+template <typename T>
+MyStack<T>::MyStack(const MyStack& stack) {
+	size = stack.size;
+	top = stack.top;
+	values = new T[size];
+	for (size_t i = 0; i < top; i++)
+		values[i] = stack.values[i];
+}
+
+template <typename T>
+T MyStack<T>::get() const {
+	if (isEmpty())
+		throw std::runtime_error("stack is empty");
+	return values[top - 1];
+}
+
+template <typename T>
+void MyStack<T>::pop() {
+	if (isEmpty())
+		throw std::runtime_error("stack is empty");
+	top--;
+}
+
+template <typename T>
+void MyStack<T>::push(const T& value) {
+	if (isFull())
+		throw std::runtime_error("stack is full");
+	values[top++] = value;
+}
+
+template <typename T>
+bool MyStack<T>::isFull() const {
+	return top == size;
+}
+
+template <typename T>
+bool MyStack<T>::isEmpty() const {
+	return top == 0;
+}
+
+template <typename T>
+MyStack<T>::~MyStack() {
+	delete[] values;
+}
+
+#endif
--- test/tests.cpp
+++ test/tests.cpp
@@ -5,7 +5,92 @@
 #include <string>
 
 TEST(MyStackTest, test1) {
-  MyStack<int> st(5);
-  EXPECT_EQ(true, st.isEmpty());
+	MyStack<int> st(5);
+	EXPECT_EQ(true, st.isEmpty());
+	MyStack<int> st(5);
+	EXPECT_EQ(true, st.isEmpty());
 }
 
+TEST(MyStackTest, test2) {
+	MyStack<int> st(5);
+	st.push(100);
+	EXPECT_EQ(100, st.get());
+}
+
+TEST(MyStackTest, test3) {
+	MyStack<int> st(5);
+	st.push(5);
+	st.push(6);
+	st.push(7);
+	st.push(8);
+	EXPECT_EQ(false, st.isEmpty());
+}
+
+TEST(StackTest, test4) {
+	MyStack<double> st(5);
+	st.push(1.5);
+	st.pop();
+	EXPECT_EQ(true, st.isEmpty());
+}
+
+TEST(MyStackTest, test5) {
+	MyStack<int> st(5);
+	EXPECT_EQ(false, st.isFull());
+}
+
+TEST(MyStackTest, test6) {
+	MyStack<int> st(5);
+	st.push(155);
+	st.push(255);
+	st.pop();
+	EXPECT_EQ(155, st.get());
+}
+
+TEST(MyStackTest, test7) {
+	MyStack<char> st(5);
+	st.push('a');
+	st.push('b');
+	st.push('c');
+	st.push('d');
+	st.push('e');
+	EXPECT_EQ(true, st.isFull());
+}
+
+TEST(MyStackTest, test8) {
+	MyStack<int> st(5);
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	st.pop();
+	st.push(4);
+	EXPECT_EQ(4, st.get());
+}
+
+TEST(MyStackTest, test9) {
+	MyStack<std::string> st(5);
+	st.push("Hello");
+	st.pop();
+	st.push("World!");
+	EXPECT_EQ("World!", st.get());
+}
+
+TEST(MyStackTest, test10) {
+	MyStack<double> st(5);
+	st.push(1.1);
+	st.push(2.2);
+	MyStack<double> st2(st);
+	st2.push(2.2);
+	st2.push(1.1);
+	EXPECT_EQ(1.1, st2.get());
+}
+
+TEST(MyStackTest, test11) {
+	MyStack<char> st(6);
+	st.push('a');
+	st.push('1');
+	st.push('b');
+	st.pop();
+	st.pop();
+	st.pop();
+	EXPECT_EQ(true, st.isEmpty());
+}
