eacc17b4a16f7ab690cf4d9e0e1c8c65eeb5e75b
MILA <milalupehina@gmail.com>
2021-03-23 16:56:52 +0300

    fix 06

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,9 +4,11 @@
 #include <gtest/gtest.h>
 #include <string>
 
+// passed
 TEST(MyStackTest, test1) {
-  MyStack<int> st(5);
-  EXPECT_EQ(true, st.isEmpty());
+  MyStack<int> st1(5);
+  EXPECT_EQ(true, st1.isEmpty());
+
 }
 
 // copied
@@ -16,85 +18,94 @@ TEST(MyStackTest, test1) {
 // isfull
 // isempty
 
+// failed
 TEST(MyStackTest, test2) {
 // isempty false
-MyStack<int> st(3);
-st.push(1);
-st.push(2);
-st.push(3);
-EXPECT_EQ(false, st.isEmpty());
-}
-
-TEST(MyStackTest, test3) {
-// isfull false
-MyStack<int> st(3);
-st.push(1);
-st.push(2);
-EXPECT_EQ(false, st.isFull());
-}
-
-TEST(MyStackTest, test4) {
-// get
-MyStack<int> st(3);
-st.push(1);
-st.push(2);
-EXPECT_EQ(2, st.get());
-}
-
-TEST(MyStackTest, test5) {
-// copied
-// get
-MyStack<int> st1(3);
-st1.push(1);
-st1.push(2);
-st1.push(4);
-MyStack<int> st2(st1);
-EXPECT_EQ(4, st2.get());
-}
-
-TEST(MyStackTest, test6) {
-// copied true
-// isfull
-MyStack<int> st1(3);
-st1.push(6);
-st1.push(5);
-st1.push(4);
-MyStack<int> st2(st1);
-EXPECT_EQ(true, st2.isFull());
-}
-
-TEST(MyStackTest, test7) {
-// copied false
-// isempty
-MyStack<int> st1(3);
-st1.push(7);
-MyStack<int> st2(st1);
+MyStack<int> st2(3);
+st2.push(1);
+st2.push(2);
+st2.push(3);
 EXPECT_EQ(false, st2.isEmpty());
 }
 
+// failed
+TEST(MyStackTest, test3) {
+// isfull false
+MyStack<int> st3(3);
+st3.push(1);
+st3.push(2);
+EXPECT_EQ(false, st3.isFull());
+}
+
+// failed
+TEST(MyStackTest, test4) {
+// get
+MyStack<int> st4(3);
+st4.push(1);
+st4.push(2);
+EXPECT_EQ(2, st4.get());
+}
+
+// failed
+TEST(MyStackTest, test5) {
+// copied
+// get
+MyStack<int> st51(3);
+st51.push(1);
+st51.push(2);
+st51.push(4);
+MyStack<int> st52(st51);
+EXPECT_EQ(4, st52.get());
+}
+
+//passed
+TEST(MyStackTest, test6) {
+// copied true
+// isfull
+MyStack<int> st61(3);
+st61.push(6);
+st61.push(5);
+st61.push(4);
+MyStack<int> st62(st61);
+EXPECT_EQ(true, st62.isFull());
+}
+
+// failed
+TEST(MyStackTest, test7) {
+// copied false
+// isempty
+MyStack<int> st71(3);
+st71.push(7);
+MyStack<int> st72(st71);
+EXPECT_EQ(false, st72.isEmpty());
+}
+
+// failed
 TEST(MyStackTest, test8) {
 // ppo second
-MyStack<double> st(3);
-st.push(7.1);
-st.push(7.2);
-st.push(7.3);
-st.pop();
-EXPECT_EQ(7.2, st.get());
+MyStack<double> st8(3);
+st8.push(7.1);
+st8.push(7.2);
+st8.push(7.3);
+st8.pop();
+EXPECT_EQ(7.2, st8.get());
 }
 
+// passed
 TEST(MyStackTest, test9) {
 // isfull true
-MyStack<char> st(3);
-st.push('a');
-st.push('b');
-st.push('c');
-EXPECT_EQ(true, st.isFull());
+MyStack<char> st9(3);
+st9.push('a');
+st9.push('b');
+st9.push('c');
+EXPECT_EQ(true, st9.isFull());
 }
 
+// passed
 TEST(MyStackTest, test10) {
 // isempty true
-MyStack<int> st(3);
-st.push(1);
-st.pop();
-EXPECT_EQ(true, st.isEmpty());
+MyStack<int> st10(3);
+st10.push(1);
+st10.pop();
+EXPECT_EQ(true, st10.isEmpty());
 }
