b93b91d95c3dfa832526db95b986e5d3befda865
MILA <milalupehina@gmail.com>
2021-03-23 16:18:27 +0300

    fix 03

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 // Copyright 2021 milalupehina
 #include "MyStack.h"
+#include <stdlib.h>
+#include <iostream>
+#include <ctime>
 
 int main() {
+    srand(time(0));
     MyStack<int> st(10);
     st.push(rand());
     st.push(rand());
