e96c6899ff0c33868a3c7f8dbcb012de531c5186
burgundypie <kotyonok2001@mai.ru>
2021-03-23 19:45:27 +0300

    Симонова Арина

--- include/MyStack.h
+++ include/MyStack.h
@@ -1,75 +1,76 @@
-#ifndef INCLUDE_MYSTACK_H_
-#define INCLUDE_MYSTACK_H_
-
+#pragma once
+#pragma once
 #include <iostream>
-#include <cstdlib>
+#include <iomanip>
 
 template <typename T>
-class MyStack {
-	T* values;
-	size_t size;
-	size_t top;
+class MyStack 
+{
+private:
+	T* stackPtr;
+	int size;
+	T top = 0;
+
 public:
-	explicit MyStack(size_t size);
-	MyStack(const MyStack& stack);
-	T get() const;
-	void pop();
-	void push(const T& value);
-	bool isFull() const;
-	bool isEmpty() const;
-	~MyStack();
+	explicit MyStack(int n_size) 
+	{
+		size = n_size;
+		stackPtr = new T[size];
+	}
+
+	MyStack(const MyStack& another_stack) 
+	{
+		size = another_stack.size;
+		stackPtr = new T[size];
+		top = another_stack.top;
+
+		for (unsigned int i = 0; i < top; i = i + 1)
+			stackPtr[i] = another_stack.stackPtr[i];
+	}
+
+	~MyStack() 
+	{
+		delete[] stackPtr;
+	}
+
+	void push(T value) 
+	{
+		stackPtr[top] = value;
+		top++;
+	}
+
+	T pop() 
+	{
+		top--;
+		return stackPtr[top];
+	}
+
+	T get() const 
+	{
+		return stackPtr[top - 1];
+	}
+
+	bool isFull() const 
+	{
+		if (top == size) 
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	}
+
+	bool isEmpty() const 
+	{
+		if (top == 0) 
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	}
 };
-
-template <typename T>
-MyStack<T>::MyStack(size_t size) {
-	this->size = size;
-	this->top = 0;
-	this->values = new T[size];
-}
-
-template <typename T>
-MyStack<T>::MyStack(const MyStack& stack) {
-	size = stack.size;
-	top = stack.top;
-	values = new T[size];
-	for (size_t i = 0; i < top; i++)
-		values[i] = stack.values[i];
-}
-
-template <typename T>
-T MyStack<T>::get() const {
-	if (isEmpty())
-		throw std::runtime_error("stack is empty");
-	return values[top - 1];
-}
-
-template <typename T>
-void MyStack<T>::pop() {
-	if (isEmpty())
-		throw std::runtime_error("stack is empty");
-	top--;
-}
-
-template <typename T>
-void MyStack<T>::push(const T& value) {
-	if (isFull())
-		throw std::runtime_error("stack is full");
-	values[top++] = value;
-}
-
-template <typename T>
-bool MyStack<T>::isFull() const {
-	return top == size;
-}
-
-template <typename T>
-bool MyStack<T>::isEmpty() const {
-	return top == 0;
-}
-
-template <typename T>
-MyStack<T>::~MyStack() {
-	delete[] values;
-}
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include "MyStack.h"
 
-int main() {
-	MyStack<int> stack(10);
-	for (int i = 0; i < 10; i++) {
-		int value = std::rand() % 10;
-		std::cout << "Push " << value << std::endl;
-		stack.push(value);
-	}
-	std::cout << std::endl << "Stack content: ";
-	while (!stack.isEmpty()) {
-		std::cout << stack.get() << " ";
-		stack.pop();
-	}
-	std::cout << std::endl;
+int main() 
+{
+	MyStack<char> st(5);
+	st.push('o');
+	st.push('l');
+	st.push('l');
+	st.push('e');
+	st.push('h');
+	MyStack <char> st2(st);
+
+	while (!st2.isEmpty())
+		std::cout << st2.pop() << " ";
+	return 0;
 }
