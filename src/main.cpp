60c43fe195489b5c21fe61467f57930c0a5f7819
6a16ec <semaev2014@yandex.ru>
2021-03-25 16:09:28 +0300

    Fixed cpplint bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,18 @@
+// Copyright [2021] <Nikita Semaev>
 #include <iostream>
 #include <string>
 #include "../include/MyStack.h"
-using namespace std;
 
-
-int main(){
-	string str = "(()((())))((()()))()";
-	MyStack <char> stack(str.size());
-	for(auto c: str){
-		if(c == '('){
-			stack.push(c);
-		}
-		else{
-			stack.pop();
-		}
-	}
-	cout << (stack.isEmpty() ? "YES" : "NO");
-	return 0;
+int main() {
+    std::string str = "(()((())))((()()))()";
+    MyStack <char> stack(str.size());
+    for (auto c : str) {
+        if (c == '(') {
+            stack.push(c);
+        } else {
+            stack.pop();
+        }
+    }
+    std::cout << (stack.isEmpty() ? "YES" : "NO");
+    return 0;
 }
